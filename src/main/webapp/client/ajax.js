5571b882e9e1221d812e72f8da8af447d0084f96
yannis <thimotheeleclerc@gmail.com>
2019-11-20 13:47:43 +0100

    add index ajax

--- src/main/webapp/client/ajax.js
+++ src/main/webapp/client/ajax.js
@@ -2,3 +2,16 @@
 function hello() {
     console.log("hello world");
 }
+
+function refresh() {
+    setTimeout("refresh();",5000);
+}
+
+var login;
+var billets;
+
+$(document).ready(function () {
+    $("#").click(function () {
+
+    })
+})
